2cf0352f4d014e1ad966a3e22e26601c2b97480d
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2023-08-04 02:51:06 -0700

    add exception handling to data layer

--- data-access/data-layer.php
+++ data-access/data-layer.php
@@ -1,20 +1,28 @@
 <?php
   function getQuery($sql) {
     include "./constants/mysql-vals.php";
-    $conn = new mysqli($servername, $username, $password, $schema);
-    if ($conn->connect_error) {
-      return false;
-    }
-    $query = $sql;
-    $result = mysqli_query($conn, $query);
-    $resultCheck = mysqli_num_rows($result);
-    $data = [];
-    if ($resultCheck > 0) {
-      while ($row = mysqli_fetch_assoc($result)) {
-        array_push($data, $row);
+    try {
+      $conn = new mysqli($servername, $username, $password, $schema);
+      if ($conn->connect_error) {
+        throw new Exception('Unable to connect to SQL server.');
       }
+      $query = $sql;
+      $result = mysqli_query($conn, $query);
+      $resultCheck = mysqli_num_rows($result);
+      $data = [];
+      if ($resultCheck > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+          array_push($data, $row);
+        }
+      }
+      $conn->close();
+      return $data;
+    } catch (Exception $e) {
+      if ($conn) {
+        $conn->close();
+      }
+      echo 'Caught exception: ',  $e->getMessage(), "\n";
+      die();
     }
-    $conn->close();
-    return $data;
   }
 ?>
